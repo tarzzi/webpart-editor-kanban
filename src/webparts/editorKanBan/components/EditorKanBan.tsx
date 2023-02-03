80f0e15dcf362fcf6fcd4114056e0e3f153c8f91
Tarmo Urrio <5585433+tarzzi@users.noreply.github.com>
2023-02-03 23:51:23 +0200

    Board start and styling

--- src/webparts/editorKanBan/components/EditorKanBan.tsx
+++ src/webparts/editorKanBan/components/EditorKanBan.tsx
@@ -2,41 +2,23 @@ import * as React from 'react';
 import styles from './EditorKanBan.module.scss';
 import { IEditorKanBanProps } from './IEditorKanBanProps';
 import { escape } from '@microsoft/sp-lodash-subset';
+import Board from './Board/Board';
 
-export default class EditorKanBan extends React.Component<IEditorKanBanProps, {}> {
+interface IEditorKanBanState {
+  siteUrl: string;
+}
+
+export default class EditorKanBan extends React.Component<IEditorKanBanProps, IEditorKanBanState> {
   public render(): React.ReactElement<IEditorKanBanProps> {
     const {
-      description,
-      isDarkTheme,
-      environmentMessage,
-      hasTeamsContext,
-      userDisplayName
+      context,
+      siteUrl
     } = this.props;
 
     return (
-      <section className={`${styles.editorKanBan} ${hasTeamsContext ? styles.teams : ''}`}>
-        <div className={styles.welcome}>
-          <img alt="" src={isDarkTheme ? require('../assets/welcome-dark.png') : require('../assets/welcome-light.png')} className={styles.welcomeImage} />
-          <h2>Well done, {escape(userDisplayName)}!</h2>
-          <div>{environmentMessage}</div>
-          <div>Web part property value: <strong>{escape(description)}</strong></div>
-        </div>
-        <div>
-          <h3>Welcome to SharePoint Framework!</h3>
-          <p>
-            The SharePoint Framework (SPFx) is a extensibility model for Microsoft Viva, Microsoft Teams and SharePoint. It&#39;s the easiest way to extend Microsoft 365 with automatic Single Sign On, automatic hosting and industry standard tooling.
-          </p>
-          <h4>Learn more about SPFx development:</h4>
-          <ul className={styles.links}>
-            <li><a href="https://aka.ms/spfx" target="_blank" rel="noreferrer">SharePoint Framework Overview</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-graph" target="_blank" rel="noreferrer">Use Microsoft Graph in your solution</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-teams" target="_blank" rel="noreferrer">Build for Microsoft Teams using SharePoint Framework</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-viva" target="_blank" rel="noreferrer">Build for Microsoft Viva Connections using SharePoint Framework</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-store" target="_blank" rel="noreferrer">Publish SharePoint Framework applications to the marketplace</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-api" target="_blank" rel="noreferrer">SharePoint Framework API reference</a></li>
-            <li><a href="https://aka.ms/m365pnp" target="_blank" rel="noreferrer">Microsoft 365 Developer Community</a></li>
-          </ul>
-        </div>
+      <section className={`${styles.editorKanBan}`}>
+        <h1>Some data her plz</h1>
+        <Board context={context} siteUrl={siteUrl} />
       </section>
     );
   }
--- src/webparts/editorKanBan/components/IEditorKanBanProps.ts
+++ src/webparts/editorKanBan/components/IEditorKanBanProps.ts
@@ -1,7 +1,10 @@
+import { WebPartContext } from "@microsoft/sp-webpart-base";
 export interface IEditorKanBanProps {
   description: string;
   isDarkTheme: boolean;
   environmentMessage: string;
   hasTeamsContext: boolean;
   userDisplayName: string;
+  context: WebPartContext;
+  siteUrl: string;
 }
